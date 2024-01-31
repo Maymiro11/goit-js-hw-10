16cc16c1b7a4a86c53704bc37d58b8a05c333b5d
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-01-31 15:52:44 +0100

    message-modified

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -10,45 +10,16 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const delayInput = document.querySelector('input[name="delay"]');
+    const delayInput = document.querySelector('input[name="delay"]').value;
     const stateInputs = document.querySelectorAll('input[name="state"]');
     const selectedState = [...stateInputs].find(input => input.checked);
 
     if (!delayInput || !selectedState) {
-        
-        iziToast.show({
-            title: 'Warning',
-            message: 'Please fill the fields',
-            class: 'warning-message',
-            position: 'topCenter',
-            titleColor: '#ffffff',
-            titleSize: '16px',
-            titleLineHeight: '1.5',
-            messageColor: '#ffffff',
-            messageSize: '16px',
-            messageLineHeight: '1.5',
-            backgroundColor: '#FFA000',
-            iconColor: '#ffffff',
-            iconUrl: warningIcon ,
-            imageWidth: 50,
-            timeout: 10000,
-            close: false,
-            buttons: [
-                [
-                `<button type="button" style="background-color: #FFA000"><img src=${closeIcon}></button>`,
-                function (instance, toast) {
-                    instance.hide({ transitionOut: 'fadeOut' }, toast);
-                },
-                ],
-            ],
-            closeOnEscape: true,
-            pauseOnHover: false,
-        });
-
+        showCustomToast('warning','Please fill the fields', 'warning-message', '#FFA000', warningIcon);
         return;
     }
 
-    const delay = parseInt(delayInput.value);
+    const delay = parseInt(delayInput);
 
         const promise = new Promise((resolve, reject) => {
             if (selectedState.value === 'fulfilled') {
@@ -61,67 +32,11 @@ form.addEventListener('submit', function(e) {
 
 promise.then(
     (delay) => {
-        
-        iziToast.show({
-            title: 'OK',
-            message: `Fulfilled promise in ${delay}ms`,
-            class: 'success-message',
-            position: 'topCenter',
-            titleColor: '#ffffff',
-            titleSize: '16px',
-            titleLineHeight: '1.5',
-            messageColor: '#ffffff',
-            messageSize: '16px',
-            messageLineHeight: '1.5',
-            backgroundColor: '#59A10D',
-            iconColor: '#ffffff',
-            iconUrl: okIcon ,
-            imageWidth: 50,
-            timeout: 10000,
-            close: false,
-            buttons: [
-                [
-                `<button type="button" style="background-color: #59A10D"><img src=${closeIcon}></button>`,
-                function (instance, toast) {
-                    instance.hide({ transitionOut: 'fadeOut' }, toast);
-                },
-                ],
-            ],
-            closeOnEscape: true,
-            pauseOnHover: false,
-        });
+        showCustomToast('OK',`Fulfilled promise in ${delay}ms`, 'success-message', '#59A10D', okIcon);
     },
 
     (delay) => {
-
-        iziToast.show({
-            title: 'Error',
-            message: `Rejected promise in ${delay}ms`,
-            class: 'error-message',
-            position: 'topCenter',
-            titleColor: '#ffffff',
-            titleSize: '16px',
-            titleLineHeight: '1.5',
-            messageColor: '#ffffff',
-            messageSize: '16px',
-            messageLineHeight: '1.5',
-            backgroundColor: '#ef4040',
-            iconColor: '#ffffff',
-            iconUrl: errorIcon ,
-            imageWidth: 50,
-            timeout: 10000,
-            close: false,
-            buttons: [
-                [
-                `<button type="button" style="background-color: #EF4040; display: inline-block; white-space: nowrap;"><img src=${closeIcon}></button>`,
-                function (instance, toast) {
-                    instance.hide({ transitionOut: 'fadeOut' }, toast);
-                },
-                ],
-            ],
-            closeOnEscape: true,
-            pauseOnHover: false,
-        });
+        showCustomToast('Error',`Rejected promise in ${delay}ms`, 'error-message', '#ef4040', errorIcon);
     }
 );
 
@@ -129,6 +44,7 @@ form.reset();
 });
 
 function showCustomToast(title, message, className, backgroundColor, iconUrl) {
+    
     iziToast.show({
         title: title,
         message: message,
@@ -148,7 +64,7 @@ function showCustomToast(title, message, className, backgroundColor, iconUrl) {
         close: false,
         buttons: [
             [
-                `<button type="button" style="background-color: ${backgroundColor}"><img src=${iconUrl}></button>`,
+                `<button type="button" style="background-color: ${backgroundColor}"><img src=${closeIcon}></button>`,
                 function (instance, toast) {
                     instance.hide({ transitionOut: 'fadeOut' }, toast);
                 },
